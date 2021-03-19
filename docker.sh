c124e6a658095388801d897317251922cdc53c79
hayabusa2 <public.yk@gmail.com>
2021-03-19 11:18:02 +0900

    No that's not what I meant

--- docker.sh
+++ docker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker run --rm -it --privileged -v $(pwd)/data:/opt/Mashiron-go/bin/data -v $(pwd)/mashiron.ini:/opt/Mas
-hiron-go/bin/mashiron.ini
+docker run --rm -it --privileged -v $(pwd)/data:/opt/Mashiron-go/bin/data -v $(pwd)/mashiron.ini:/opt/Mashiron-go/bin/mashiron.ini
